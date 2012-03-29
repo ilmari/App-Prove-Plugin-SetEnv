30c8693c5043e4585a8437ec2ddf34c7c5c41530
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2012-03-29 13:38:33 +0100

    Initial version

---
+++ lib/App/Prove/Plugin/SetEnv.pm
@@ -0,0 +1,38 @@
+package App::Prove::Plugin::SetEnv;
+
+# ABSTRACT: a prove plugin to set environment variables
+
+use strict;
+use warnings;
+
+sub load {
+    my ($class, $p) = @_;
+    foreach my $arg (@{$p->{args}}) {
+        my ($var, $val) = split '=', $arg, 2;
+        $ENV{$var} = $val;
+    }
+}
+
+=head1 SYNOPSIS
+
+    # command-line usage
+    prove -PSetEnv=TZ=UTC,PATH="./bin:$PATH"
+
+=head1 DESCRIPTION
+
+This L<prove> plugin lets you set environment variables for your test
+scripts.  It is particularly handy in C<.proverc>.
+
+=head1 BUGS
+
+Due to the way L<App::Prove> splits argumets to plugins, it is not
+possible to set values containing commas.
+
+=head1 SEE ALSO
+
+L<prove>, L<App::Prove/PLUGINS>.
+
+
+=cut
+
+1;
